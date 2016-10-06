b8dc2b11dbf1df08c2eef762fea7ed1aed42f3fd
Ben Tristem <github@bentristem.com>
2016-10-06 17:54:09 +0100

    YAHOO! Using Atom (with plugin), and Python Console in Blender only...
    + Note the Blender typical conditional import / reload pattern
    + See slides for one-line setup of Python Console

---
+++ move.py
@@ -0,0 +1,6 @@
+import bpy
+
+def move_all(amount):
+    scene = bpy.context.scene
+    for obj in scene.objects:
+        obj.location.x -= amount
--- plaques.py
+++ plaques.py
@@ -1,20 +1,27 @@
 # Makes plaques, as in things on walls
 # Can probably make more than 2000
 
+import sys
+
+# Ensure that dependencies are up-to-date
+if "bpy" in locals():
+    print("Reloading")
+    import importlib
+    importlib.reload(move)
+else:
+    print("Importing")
+    import move
+
 import bpy
 
 bl_info = {"name": "My Test Addon", "category": "Object"}
 
 def register():
     print("PlaqueMaker 2000 Starting Up")
-    print("Work your magic")
+    move.move_all(0.2)
+
 def unregister():
     print("PlaqueMaker 2000 Shutting Down")
-
-# scene = bpy.context.scene
-# for obj in scene.objects:
-#     obj.location.x -= 1.0
-#     print("I did it, I did it")
 
 if __name__ == '__main__':
     register() # So that we can run the code from Text Editor
