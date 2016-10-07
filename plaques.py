9f7e1b4b6ed7bd5f43c22cf315f2c219013ac349
Ben Tristem <github@bentristem.com>
2016-10-07 12:44:51 +0100

    Read names.csv from current folder, and print to console

--- plaques.py
+++ plaques.py
@@ -1,7 +1,16 @@
 # Makes plaques, as in things on walls
 # Can probably make more than 2000
 
+# Setup: import os, sys, importlib ; sys.path.append(os.path.dirname(bpy.data.filepath)) ; import plaques
+# Reload: importlib.reload(plaques)
+
+# System libraries
+import csv
 import sys
+
+# 3rd Party libraries
+
+# Own modules
 
 # Ensure that dependencies are up-to-date
 if "move" in locals():
@@ -12,16 +21,24 @@ else:
     print("Importing")
     import move
 
-import bpy
-
+# Parsed by Blender, use only strings or the world explodes
 bl_info = {"name": "My Test Addon", "category": "Object"}
 
 def register():
-    print("PlaqueMaker 2000 Starting Up")
-    move.move_all(0.2)
+    pass
 
 def unregister():
-    print("PlaqueMaker 2000 Shutting Down")
+    pass
+
+def open_csv():
+    with open('names.csv', 'r') as csvfile:
+        iterable_lazy_reader = csv.reader(csvfile, quotechar="'")
+        headers = next(iterable_lazy_reader) # consumes first item
+
+        # zip into a dictionary
+        for row in iterable_lazy_reader:
+            backer = dict(zip(headers, row))
+            print(backer)
 
 if __name__ == '__main__':
     register() # So that we can run the code from Text Editor
